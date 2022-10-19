cba96a4ccdeae010990d57a7aa4ecc8b2e326eed
ukuli piete <pieteukuri@gmail.com>
2022-10-19 04:52:07 +0200

    second commit

---
+++ styles/index.js
@@ -0,0 +1,21 @@
+function button(){
+    var error = document.getElementById("error") ;
+    var input= document.getElementById("input") ;
+    if((document.getElementById("error"))!=input.value){
+        error.innerHTML="<h6> please enter your name </h6>"
+        error.style.color="red";
+        input.style=[border="1px solid red",width ="300px" , height="50px" ];
+    
+       
+    // }else((document.getElementById("error"))==input.value); {
+    //     error.innerHTML=""
+    // }
+        
+    }
+ 
+
+    // var error1 = document.getElementById("error1");
+
+    
+    // var input1= document.getElementById("input1")
+}
